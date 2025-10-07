c9e1db9d4db1cbaa4a47ea2705ee446d271d2086
LeoBou <120125145+LeoBou@users.noreply.github.com>
2025-10-07 08:45:33 -0400

    ajout debut

--- Assets/Hummingbird/Scripts/Flower.cs
+++ Assets/Hummingbird/Scripts/Flower.cs
@@ -2,15 +2,60 @@ using UnityEngine;
 
 public class Flower : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Tooltip("The color when the flower is full")]
+    public Color fullFlowerColor = new Color(1f, 0f, .3f);
+
+    [Tooltip("The color wen the flower is empty")]
+    public Color emptyFlowerColor = new Color(.5f, 0f, 1f);
+
+    /// <summary>
+    /// The trigger collider representing the nectar
+    /// </summary>
+    [HideInInspector]
+    public Collider nectarCollider;
+
+    // The solid collider representing the flower petals
+    private Collider flowerCollider;
+
+    // The flower's material
+    private Material flowerMaterial;
+
+
+    /// <summary>
+    /// A vector pointing straight out of the flower
+    /// </summary>
+    public Vector3 FlowerUpVector
     {
-        
+        get
+        {
+            return nectarCollider.transform.up;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// The center position of the nectar collider
+    /// </summary>
+    public Vector3 FlowerCenterPosition
     {
-        
+        get
+        {
+            return nectarCollider.transform.position;
+        }
+    }
+
+    /// <summary>
+    /// The amount of nectar remaining in the flower
+    /// </summary>
+    public float NectarAmount { get; private set; }
+
+    /// <summary>
+    /// Whether the flower has any nectar remaining
+    /// </summary>
+    public bool HasNectar
+    {
+        get
+        {
+            return NectarAmount > 0f;
+        }
     }
 }
